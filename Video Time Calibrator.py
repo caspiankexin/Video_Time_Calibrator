d8d23785edcd4082e2907a3e8f0749b7ac6e025f
caspiankexin <chaiyuke2013@163.com>
2025-03-01 21:35:38 +0800

    Update Video Time Calibrator.py

--- Video Time Calibrator.py
+++ Video Time Calibrator.py
@@ -10,12 +10,10 @@ import subprocess
 from datetime import datetime
 import pytz
 from pathlib import Path
-from tkinter import filedialog, scrolledtext
 import os
 import tkinter as tk
-from tkinter import ttk
 from PIL import Image, ImageTk
-
+import sys
 
 def convertDate(MediaCreateDateText):
     # 截取日期部分
@@ -50,9 +48,14 @@ def get_metadata(file_path, type):
         print(f"Error: {result.stderr}")
 
 def process_videos():
-    folder_path = Path(folder_path_var.get())
-    log_text.delete(1.0, tk.END)
-    log_text.insert(tk.END, "开始遍历文件夹...\n")
+
+    print("欢迎使用本程序！")
+    print("软件名称：视频时间校准统一工具（Name: Video Time Calibrator）\n编写时间：2025年2月28日")
+    print("软件版本：1.0.0\n作者GitHub：@caspiankexin\n作者邮箱：mirror_flower @ outlook.com")
+    print("注意：请做好文件备份，以免丢失!\n版权：免费开源、不得商用!\n如果对您有用，感谢进行打赏!!!\n\n")
+
+
+    folder_path = Path(input("请输入需处理视频的文件夹路径："))
     for file_path in folder_path.rglob("*"):
         if file_path.is_file():
             if file_path.suffix in [".mp4"]:
@@ -60,7 +63,7 @@ def process_videos():
                 MediaCreateDateStr = get_metadata(file_path.resolve(), file_path.suffix)
                 if MediaCreateDateStr:
                     if "0000" in MediaCreateDateStr:
-                        log_text.insert(tk.END, '文件' + file_path.name + "媒体创建日期无效" + "\n")
+                        print('文件' + file_path.name + "媒体创建日期无效")
                         new_filename = "无效日期 " + file_path.name
                         new_path = file_path.parent / new_filename
                         os.rename(file_path, new_path)
@@ -68,9 +71,16 @@ def process_videos():
 
                     localDate = convertDate(MediaCreateDateStr)
 
-                    # 使用 exiftool 将 CreateDate 写入到 File Modification Date/Time 和 File Creation Date/Time 和 File Access Date/Time 以及所有的时间元素里面
+                    # 使用 exiftool 将 CreateDate 写入到 File Modification Date/Time 和 File Creation Date/Time 以及 File Access Date/Time 以及所有的时间元素里面
                     subprocess.run(
-                        ["exiftool", "-overwrite_original", "-FileModifyDate=" + localDate.strftime("%Y:%m:%d %H:%M:%S"), "-FileCreateDate=" + localDate.strftime("%Y:%m:%d %H:%M:%S"), "-ModifyDate=" + localDate.strftime("%Y:%m:%d %H:%M:%S"), "-TrackCreateDate=" + localDate.strftime("%Y:%m:%d %H:%M:%S"), "-TrackModifyDate=" + localDate.strftime("%Y:%m:%d %H:%M:%S"), "-MediaCreateDate =" + localDate.strftime("%Y:%m:%d %H:%M:%S"), "-MediaModifyDate =" + localDate.strftime("%Y:%m:%d %H:%M:%S"), file_path],
+                        ["exiftool", "-overwrite_original",
+                         "-FileModifyDate=" + localDate.strftime("%Y:%m:%d %H:%M:%S"),
+                         "-FileCreateDate=" + localDate.strftime("%Y:%m:%d %H:%M:%S"),
+                         "-ModifyDate=" + localDate.strftime("%Y:%m:%d %H:%M:%S"),
+                         "-TrackCreateDate=" + localDate.strftime("%Y:%m:%d %H:%M:%S"),
+                         "-TrackModifyDate=" + localDate.strftime("%Y:%m:%d %H:%M:%S"),
+                         "-MediaCreateDate =" + localDate.strftime("%Y:%m:%d %H:%M:%S"),
+                         "-MediaModifyDate =" + localDate.strftime("%Y:%m:%d %H:%M:%S"), file_path],
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE,
                         text=True,
@@ -80,62 +90,56 @@ def process_videos():
                     new_filename = localDate.strftime("%Y%m%d") + "_" + file_path.name
                     new_path = file_path.parent / new_filename
                     os.rename(file_path, new_path)
-                    log_text.insert(tk.END, f"文件 {file_path.name} 已重命名为 {new_filename}\n")
+                    print(f"文件 {file_path.name} 已处理完成！")
 
                 else:
-                    log_text.insert(tk.END, "该文件没有找到媒体创建日期：" + file_path.name + "\n")
-    log_text.insert(tk.END, "\n" + "文件夹下所有mp4文件，已经全部处理完毕！" + "\n" + "文件夹下所有mp4文件，已经全部处理完毕！" + "\n" + "文件夹下所有mp4文件，已经全部处理完毕！" + "\n")
+                    print("该文件没有找到媒体创建日期：" + file_path.name)
+    print("\n" + "文件夹下所有mp4文件，已经全部处理完毕！" + "\n")
 
+def show_image_window():
+    # 创建一个新的Toplevel窗口
+    image_window = tk.Toplevel(root)
+    image_window.title("赞赏码")
+    image_window.attributes("-topmost", True)  # 设置窗口为最上层
 
+    # 获取屏幕尺寸
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
 
-def browse_folder():
-    folder_selected = filedialog.askdirectory()
-    folder_path_var.set(folder_selected)
+    # 设置弹窗尺寸为屏幕的30%
+    window_width = int(screen_width * 0.3)
+    window_height = int(screen_height * 0.45)
 
-# 创建主窗口
-root = tk.Tk()
-root.title("视频时间校准统一工具（Video Time Calibrator）")
+    # 计算弹窗位置，使其居中显示
+    x = (screen_width // 2) - (window_width // 2)
+    y = (screen_height // 2) - (window_height // 2)
 
+    # 设置弹窗尺寸和位置
+    image_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-# 获取屏幕尺寸
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+    # 加载PNG图片
+    image = Image.open(get_resource_path('images/赞赏码.png'))
+    image = image.resize((300, 300), Image.LANCZOS)  # 调整图片大小
+    photo = ImageTk.PhotoImage(image)
 
-# 设置窗口尺寸为屏幕的60%
-window_width = int(screen_width * 0.6)
-window_height = int(screen_height * 0.6)
+    # 创建一个Label组件来显示图片
+    image_label = tk.Label(image_window, image=photo)
+    image_label.image = photo  # 保持对图片的引用
+    image_label.pack(pady=10)
 
-# 计算窗口位置，使其居中显示
-x = (screen_width // 2) - (window_width // 2)
-y = (screen_height // 2) - (window_height // 2)
+    # 创建一个Label组件来显示文字
+    text_label = tk.Label(image_window, text="如果对您有用，感谢进行打赏。", font=('Helvetica', 12))
+    text_label.pack(pady=10)
 
-# 设置窗口尺寸和位置
-root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+def get_resource_path(relative_path):
+    # 用于打包含有图片的Python程序
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
-# 修改布局，使软件信息区域在右侧
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(2, weight=1)
-
-# 文件夹路径输入区域
-folder_path_var = tk.StringVar()
-folder_path_entry = tk.Entry(root, textvariable=folder_path_var, width=50)
-folder_path_entry.grid(row=0, column=0, padx=10, pady=10, sticky='ew')
-browse_button = tk.Button(root, text="浏览", command=browse_folder, font=('Helvetica', 12))
-browse_button.grid(row=0, column=1, padx=10, pady=10, sticky='ew')
-
-# 程序运行状态显示区域
-log_text = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=80, height=28)
-log_text.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky='nsew')
-log_text.configure(yscrollcommand=True)
-
-# 软件信息介绍区域
-info_label = tk.Label(root, text="软件名称：视频时间校准统一工具\n\nName: Video Time Calibrator\n\n软件版本：1.0.0\n\n作者GitHub：@caspiankexin\n\n作者邮箱：mirror_flower@outlook.com\n\n编写时间：2025年2月28日\n\n注意：请做好文件备份，以免丢失\n\n版权：免费开源、不得商用\n\n如果对您有用，感谢进行打赏!", font=('Helvetica', 11), anchor='w')
-info_label.grid(row=0, column=2, rowspan=2, padx=10, pady=10, sticky='nsew')
-
-
-# 开始按钮
-start_button = tk.Button(root, text="开始处理", command=process_videos, font=('Helvetica', 12))
-start_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10, sticky='ew')
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()  # 隐藏主窗口
+    show_image_window()
+    process_videos()
+    input("\n\n按任意键退出程序...")
